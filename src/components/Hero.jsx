e15492c22252ada24651a3b2a846c6c0e656952d
Ahsans-code <ahsan326ali@gmail.com>
2025-01-10 23:41:08 +0500

    hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,14 @@ const Hero = () => {
   return (
     <div
       id="home"
-      className="relative max-md:pt-32 p-4 sm:pt-32 sm:p-8 lg:h-[500px] h-[90vh] flex flex-col md:justify-center"
+      className="relative pt-7  p-6 sm:pt-32 sm:p-8 lg:h-[100vh] h-[84vh] flex flex-col md:justify-center"
     >
       {/* Background Layer */}
 
       <div
-        className="absolute inset-0 bg-cover bg-center"
+        className="absolute inset-0 bg-cover bg-center shadow-box-shadow-mobile md:shadow-box-shadow-desktop"
         style={{
-          boxShadow:
-            "inset 0 -100px 180px #1a1a19, inset 0 300px 130px #1a1a19",
+        
 
           backgroundImage:
             "url('https://cdn.prod.website-files.com/66964d12f0212962622e6bd4/669770eddbcf5f504ef99354_hero-01.jpg')",
@@ -23,17 +22,17 @@ const Hero = () => {
       <div className="relative z-10">
         {/* Top Section */}
         <div className="sm:px-[3rem] flex flex-col lg:flex-row items-start gap-8 lg:gap-[10rem] justify-between">
-          <div className="text-2xl sm:text-4xl lg:text-5xl font-semibold sm:text-center lg:text-left">
+          <div className="text-4xl lg:text-5xl font-semibold sm:text-center lg:text-left">
             Optimize and monetize social media marketing
           </div>
-          <div className="text-sm sm:text-base lg:text-md sm:text-center lg:text-left">
+          <div className="text-2xl sm:text-center lg:text-left text-[#aca0cf]">
             Sociallywiredinc boosts creators' reach with curated content and
             tailored social media strategies for targeted audience growth.
           </div>
         </div>
         {/* Bottom Section */}
         <div
-          className="mt-8 pb-8 sm:pb-16 lg:mt-16 text-[3.3rem] sm:text-8xl lg:text-[12rem] font-bold sm:text-center text-transparent bg-clip-text"
+          className="mt-8 pb-8 sm:pb-16 lg:mt-16 text-[3.6rem]  sm:text-center  sm:text-8xl lg:text-[12rem] font-bold  text-transparent bg-clip-text"
           style={{
             backgroundImage:
               "linear-gradient(160deg, #a388f5, #6ae8ff 22%, #fff694 52%, #fc67ff 83%)",
@@ -42,7 +41,7 @@ const Hero = () => {
           SociallyWired
         </div>
         <div className="flex justify-center items-center ">
-          {/* <button
+          <button
             className=" relative p-[2px] rounded-md"
             style={{
               background:
@@ -50,14 +49,14 @@ const Hero = () => {
             }}
           >
             <span
-              className="block bg-[#1B1B1A] px-4 py-2 rounded-md hover:bg-transparent transition-all duration-300"
+              className="block text-xl bg-[#1B1B1A] px-5 py-3 rounded-md hover:bg-transparent transition-all duration-300"
               style={{
                 backgroundClip: "padding-box",
               }}
             >
               Request A Quote{" "}
             </span>
-          </button> */}
+          </button>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
       },
       backgroundImage: {
         'custom-gradient': 'linear-gradient(to bottom, #fff500, #9c4ffe)',
+        
       },
+      boxShadow:{"box-shadow-mobile":" inset 0px -13px 350px 100px rgba(0,0,0,1)",
+        "box-shadow-desktop":"inset 0 -50px 50px #1a1a19, inset 0 200px 120px #1a1a19"
+      }
     },
   },
   plugins: [],
